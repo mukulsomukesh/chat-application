1ee9e4edcde58776e2bb78bf91bb8ec51ab6b42d
mukulsomukesh <mukulsomukesh@gmail.com>
2023-06-26 00:36:22 +0530

    created api for rename chat group

--- backend/controller/chat.controller.js
+++ backend/controller/chat.controller.js
@@ -56,14 +56,14 @@ const getChats = async (req, res) => {
 // create group chat 
 const createGroupChat = asyncHandler(async (req, res) => {
 
-    if(!req.body.users && !req.body.name){
-        return res.status(422).json({error:"Please fill all the details."})
+    if (!req.body.users && !req.body.name) {
+        return res.status(422).json({ error: "Please fill all the details." })
     }
 
     let users = JSON.parse(req.body.users);
 
-    if(users.length<2){
-        return res.status(422).json({error: "Minimum two users are required to create a group."})
+    if (users.length < 2) {
+        return res.status(422).json({ error: "Minimum two users are required to create a group." })
     }
 
     users.push(req.user);
@@ -73,20 +73,20 @@ const createGroupChat = asyncHandler(async (req, res) => {
         const existingChat = await Chat.findOne({
             users: { $all: users },
             isGroupChat: true
-        });
+        }).populate("users", "-password").populate("groupAdmin", "-password");
 
         if (existingChat) {
             return res.status(200).json(existingChat);
         }
 
         const chatGroup = await Chat.create({
-            chatName:req.body.name,
-            users:users,
-            isGroupChat:true,
-            groupAdmin:req.user
+            chatName: req.body.name,
+            users: users,
+            isGroupChat: true,
+            groupAdmin: req.user
         })
 
-        const groupChatObj = await Chat.findOne({_id: chatGroup._id}).populate("users","-password").populate("groupAdmin", "-pasdsword")
+        const groupChatObj = await Chat.findOne({ _id: chatGroup._id }).populate("users", "-password").populate("groupAdmin", "-pasdsword")
         res.status(201).json(groupChatObj);
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -95,6 +95,25 @@ const createGroupChat = asyncHandler(async (req, res) => {
 
 // rename group chat
 const renameGroup = asyncHandler(async (req, res) => {
+
+    const { chatId, chatName } = req.body;
+
+    if (!chatName) {
+        res.status(422).json({ error: "Please Enter New Name." })
+    }
+
+    const newChatObj = await Chat.findByIdAndUpdate(
+        chatId,
+        { chatName, },
+        { new: true }
+    ).populate("users", "-password").populate("groupAdmin", "-password")
+
+    if (!newChatObj) {
+        res.status(404).json({ error: "Chat Not Found." })
+    }
+    else {
+        res.status(200).json(newChatObj)
+    }
 
 })
 
--- backend/routes/chat.routes.js
+++ backend/routes/chat.routes.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const { authUserMiddleware } = require("../middlewares/auth.middleware");
-const { accessChat, getChats, createGroupChat } = require("../controller/chat.controller");
+const { accessChat, getChats, createGroupChat, renameGroup } = require("../controller/chat.controller");
 
 const router = express.Router()
 
 router.route("/").post(authUserMiddleware, accessChat)
 router.route("/").get(authUserMiddleware,getChats)
 router.route("/group").post(authUserMiddleware, createGroupChat)
-// router.route("/rename").post(authUserMiddleware, )
+router.route("/rename").put(authUserMiddleware, renameGroup)
 // router.route("/groupRemove").post(authUserMiddleware, )
 // router.route("/groupAdd").post(authUserMiddleware, )
 
