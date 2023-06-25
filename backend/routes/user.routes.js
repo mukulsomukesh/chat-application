37074c6b0d89cde50a31ac4ad1aee6adea953e3d
mukulsomukesh <mukulsomukesh@gmail.com>
2023-06-25 16:58:34 +0530

    created search_user api & auth middleware

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { registerUser, authUser } = require("../controller/user.controller");
+const { registerUser, authUser, searchUser } = require("../controller/user.controller");
+const { authUserMiddleware } = require("../middlewares/auth.middleware");
 
 const router = express.Router();
 
-router.route("/").post(registerUser);
+router.route("/").post(registerUser).get( authUserMiddleware ,searchUser);
 router.route("/login").post(authUser);
 
 module.exports = router;
