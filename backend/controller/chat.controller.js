6b2dbd6acc173a6a291a32d9903b685636c2f8ca
mukulsomukesh <mukulsomukesh@gmail.com>
2023-06-25 18:18:25 +0530

    created api for get user chat

--- backend/controller/chat.controller.js
+++ backend/controller/chat.controller.js
@@ -1,76 +1,76 @@
 const asyncHandler = require("express-async-handler");
 const Chat = require("../models/chat.model");
 const User = require("../models/user.model")
+
 // access chat
-const accessChat = asyncHandler(async (req, res)=>{
-  
-    const {userId} = req.body;
-console.log(userId)
-    if(!userId){
-       return res.status(500).json({error:"userId not present in req body."})
+const accessChat = async (req, res) => {
+    const { userId } = req.body;
+
+    if (!userId) {
+        return res.status(400).json({ error: "userId not present in the request body." });
     }
 
-    let isChat = await Chat.find({
-        isChatGroup: false,
-        $and:[
-            {users: {$elemMatch: {$eq: req.user._id}}},
-            {users: {$elemMatch: {$eq: userId}}},
-        ]
-    }).populate("users","-password").populate("latestMessage");
+    try {
+        const existingChat = await Chat.findOne({
+            isGroupChat: false,
+            users: { $all: [req.user._id, userId] }
+        }).populate("users", "-password").populate("latestMessage").populate("latestMessage.sender");
 
-    isChat = await User.populate(isChat, {
-        path:"latestMessage.sender",
-        select:"name pic email",
-    })
-    
-    if(isChat.length>0){
-        res.send(isChat[0])
-    }
-    else{
-        let chatData = {
+        if (existingChat) {
+            return res.status(200).json(existingChat);
+        }
+
+        const newChat = new Chat({
             chatName: "sender",
             isGroupChat: false,
-            users:[req.user._id, userId],
-        }
+            users: [req.user._id, userId]
+        });
 
-        try {
-            const createChat = await Chat.create(chatData);
+        await newChat.save();
 
-            const fullChat = await Chat.findOne({_id: createChat._id}).populate("users","-password");
+        const populatedChat = await Chat.findOne({ _id: newChat._id }).populate("users", "-password");
 
-            res.status(200).json(fullChat);
-
-        } catch (error) {
-            res.status(500).json({error:error.message});
-        }
-
+        res.status(201).json(populatedChat);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
-    
-}) 
+};
+
 
 // get chat
-const getChats = asyncHandler(async (req, res)=>{
-    
-})
+const getChats = async (req, res) => {
+    try {
+        const chats = await Chat.find({ users: { $elemMatch: { $eq: req.user._id } } });
+
+        if (chats.length === 0) {
+            return res.status(422).json({ message: "No chats found." });
+        }
+
+        res.status(200).json(chats);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
 
 // create group chat 
-const createGroupChat = asyncHandler(async (req, res)=>{
-    
+const createGroupChat = asyncHandler(async (req, res) => {
+
 })
 
 // rename group chat
-const renameGroup = asyncHandler(async (req, res)=>{
-    
+const renameGroup = asyncHandler(async (req, res) => {
+
 })
 
 // remove other user from group chat
-const removeFromGroup = asyncHandler(async (req, res)=>{
-    
+const removeFromGroup = asyncHandler(async (req, res) => {
+
 })
 
 // add other user to chat group
-const addToGroup = asyncHandler(async (req, res)=>{
-    
+const addToGroup = asyncHandler(async (req, res) => {
+
 })
 
 module.exports = { accessChat, getChats, createGroupChat, renameGroup, removeFromGroup, addToGroup };
--- backend/routes/chat.routes.js
+++ backend/routes/chat.routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const { authUserMiddleware } = require("../middlewares/auth.middleware");
-const { accessChat } = require("../controller/chat.controller");
+const { accessChat, getChats } = require("../controller/chat.controller");
 
 const router = express.Router()
 
 router.route("/").post(authUserMiddleware, accessChat)
-// router.route("/").get(authUserMiddleware,)
+router.route("/").get(authUserMiddleware,getChats)
 // router.route("/group").post(authUserMiddleware, )
 // router.route("/rename").post(authUserMiddleware, )
 // router.route("/groupRemove").post(authUserMiddleware, )
