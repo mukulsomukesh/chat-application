335405bb24b24f11f6cbd28e41b28ef55e2f0b5e
mukulsomukesh <mukulsomukesh@gmail.com>
2023-07-01 03:07:03 +0530

    fix wrong user name display

--- frontend/src/components/Home/AllChats/DisplayChatCard.jsx
+++ frontend/src/components/Home/AllChats/DisplayChatCard.jsx
@@ -3,27 +3,27 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getMessage, selectUserForChat } from '../../../redux/appReducer/action';
 
 const DisplayChatCard = ({ item }) => {
-
   const parsedData = JSON.parse(localStorage.getItem('chat-app-login-user-data'));
-  
   const dispatch = useDispatch();
 
   // Select User For Chat
   const handelSelectUserForChat = () => {
     dispatch(selectUserForChat(item));
-    
-    dispatch(getMessage(item._id))
-  }
+    dispatch(getMessage(item._id));
+  };
+
+  // Find the user from item that doesn't match the _id in parsedData
+  const selectedUser = item.users.find(user => user._id !== parsedData._id);
 
   return (
     <div onClick={handelSelectUserForChat} className="bg-primary-100 cursor-pointer rounded-lg shadow-md mt-3 hover:shadow-lg hover:ring-2 hover:ring-primary-200 hover:bg-primary-200 transition-shadow duration-200">
       <div className="flex items-center space-x-4 p-2">
         <div className="flex-shrink-0">
-            <img className="w-10 h-10 rounded-full" src={item.isGroupChat? "https://cdn-icons-png.flaticon.com/512/2043/2043173.png": item.users[1].pic} alt={`${item.name} image`} />
+            <img className="w-10 h-10 rounded-full" src={item.isGroupChat ? "https://cdn-icons-png.flaticon.com/512/2043/2043173.png" : selectedUser.pic} alt={`${item.name} image`} />
         </div>
         <div className="flex-1 min-w-0">
           <p className="text-xs font-medium text-primary-900 truncate dark:text-white">
-            {item.isGroupChat? item.chatName: item.users[1].name}
+            {item.isGroupChat ? item.chatName : selectedUser.name}
           </p>
           <p className="text-xs text-primary-500 truncate dark:text-primary-400">
             {item.email}
--- frontend/src/components/Home/ChatBox/ChatHeader.jsx
+++ frontend/src/components/Home/ChatBox/ChatHeader.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import UserDetails from './UserDetails'
+import React from 'react';
+import UserDetails from './UserDetails';
 import { useSelector } from 'react-redux';
 
 export default function ChatHeader() {
-
   const selectedUserForChat = useSelector((state) => state.appReducer.selectedUserForChat);
+  const localStorageData = JSON.parse(localStorage.getItem('chat-app-login-user-data'));
+  const selectedUserId = selectedUserForChat._id;
+
+  // Find the user from selectedUserForChat that doesn't match the _id in localStorageData
+  const selectedUser = selectedUserForChat.users.find(user => user._id !== localStorageData._id);
 
   return (
-    <div className='flex mt-2 justify-between items-center h-12 text-primary-50 bg-primary-800 rounded-tl-lg rounded-tr-lg  px-5' >
+    <div className='flex mt-2 justify-between items-center h-12 text-primary-50 bg-primary-800 rounded-tl-lg rounded-tr-lg px-5'>
         
-        <div className="flex items-center pt-2  ml-2 gap-2">
+      <div className="flex items-center pt-2 ml-2 gap-2">
         <div className={`bg-primary-50 rounded-full w-9 h-9 flex items-center justify-center`}>
-        <img className="w-8 h-8 rounded-full" src={selectedUserForChat.isGroupChat ? "https://cdn-icons-png.flaticon.com/512/2043/2043173.png": selectedUserForChat.users[1].pic} alt={"not found"} />
-</div>
-        <h1 className='text-2xl'>  {selectedUserForChat.isGroupChat ? selectedUserForChat.chatName: selectedUserForChat.users[1].name} </h1>
+          <img className="w-8 h-8 rounded-full" src={selectedUserForChat.isGroupChat ? "https://cdn-icons-png.flaticon.com/512/2043/2043173.png" : selectedUser.pic} alt={"not found"} />
         </div>
+        <h1 className='text-2xl'>{selectedUserForChat.isGroupChat ? selectedUserForChat.chatName : selectedUser.name}</h1>
+      </div>
 
-        <UserDetails />
+      <UserDetails />
 
     </div>
-  )
+  );
 }
