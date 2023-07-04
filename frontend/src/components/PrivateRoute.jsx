1779e150aa8eb90b005976218a5e8869f320fb86
mukulsomukesh <mukulsomukesh@gmail.com>
2023-07-04 15:58:21 +0530

    auto authenticate login user on page reload

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
+import { signInAccount } from '../redux/authReducer/action';
+import * as types from "../redux/authReducer/actionType";
 
 export default function PrivateRoute({children}) {
 
     const sign_in_success = useSelector((state)=> state.authReducer.sign_in_success);
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        console.log("private")
+        if (localStorage.getItem('chat-app-login-user-data') !== null) {
+            dispatch({ type: types.SIGN_IN_REQUEST_SUCCESS, payload: "Login Success." });
+          }
+    })
 
     if(!sign_in_success){
  
